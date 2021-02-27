53f2f2b0648f5222fb59f7fd204842c1f12f4f3a
Ahmed Wael <ahmedwaela25@gmail.com>
2021-02-27 21:08:53 +0200

    Adding validation to adding new contact

--- Phone_Book/script.js
+++ Phone_Book/script.js
@@ -6,55 +6,61 @@ var phoneField = $('#phoneField');
 var emailField = $('#emailField');
 var genderField = $('#genderField');
 
-
-function Contact(id, name, phone, email, gender) {
+function Contact(id, name, phone, email, gender, imgUrl) {
     this.id = id;
     this.name = name;
     this.phone = phone;
     this.email = email;
     this.gender = gender;
+    this.imgUrl = imgUrl;
 }
-
-
 
 
 
 window.onload = function () {
 
-    contactList = fetchContactsFromStorage();
-    
+    fetchContactsFromStorage();
+
     //Display All Contacts From Page , Given The List
-    displayAllContacts(contactList);
 
 }
-function fetchContactsFromStorage(){
+
+function fetchContactsFromStorage() {
     //Get List from local storage
     //if it exists, init pagee
     //Also Update last Id, so that subsequent entries
     //Are valid
     //if not dont do a thing
-    var contacts = JSON.parse(localStorage.getItem("contacts"));
-    
-    if (contacts !== null){
-        lastId = contacts.length;
-        return contacts;
+    contactList = JSON.parse(localStorage.getItem("contacts"));
+    console.log(contactList);
+    if (contactList != null) {
+        console.log("Found contacts in storage")
+
+        lastId = contactList[contactList.length - 1].id + 1;
+        displayAllContacts(contactList);
+        console.log("LAST ID" + lastId);
+    } else {
+        console.log("No contacts in storage")
+        contactList = [];
     }
-    else{
-        return [];
+
+}
+
+function displayAllContacts(list) {
+
+    var ulList = $('#allContacts');
+
+    ulList.empty();
+
+    for (var i = 0; i < list.length; i++) {
+
+        var html = " <li id='" + list[i].id + "' class='contact' data-filtertext='" + list[i].name + "'><a href='#'><img class='contactAvatar' src='" + list[i].imgUrl + "'><h2>" + list[i].name + "</h2></a> <a href='tel:"+list[i].phone+"' data-rel='popup' data-position-to='window' data-transition='pop'>Purchase album</a></li >";
+
+        ulList.append(html).listview("refresh");
     }
 
 }
 
-function displayAllContacts(list){
-    
-    
-    
-}
-
-function initContacts() {
-
-
-}
 
 
 function addContact() {
@@ -65,25 +71,76 @@ function addContact() {
     var phone = phoneField.val();
     var email = emailField.val();
     var gender = genderField.val();
+    var imgUrl = "";
+
+    if (gender == "male") {
+        imgUrl = "./assets/cyber.jpg";
+    } else if (gender == "female") {
+        imgUrl = "./assets/van.jpg";
+    }
+
+    var isFormValid = true;
+
+
+    if (name == "") {
+        $("#nameFieldErrArea").text("Name Is required");
+        isFormValid = false;
+    }else{
+        $("#nameFieldErrArea").empty();
+    }
+    if (phone == "") {
+
+        $("#phoneFieldErrArea").text("Name Is required");
+        isFormValid = false;
+    } else if (!isPhoneValid(phone)) {
+        $("#phoneFieldErrArea").text("Phone is not valid");
+        isFormValid = false;
+    }else{
+        $("#phoneFieldErrArea").empty();
+    }
+
+    if (email == "") {
+        $("#emailFieldErrArea").text("Name Is required");
+        isFormValid = false;
+    } else if (!isEmailValid(email)) {
+        $("#emailFieldErrArea").text("Email Is not valid");
+        isFormValid = false;
+    }else{
+        $("#emailFieldErrArea").empty();
+    }
+
+    if(!isFormValid){
+        return;
+    }
+
+
+
+
 
     //Contact Should take last avail ID
-    var newContact = new Contact(1, name, phone, email, gender);
+    var newContact = new Contact(lastId++, name, phone, email, gender, imgUrl);
 
-    console.log(newContact);
-
-    
-    var s = JSON.stringify(newContact);
-    
-    console.log(s);
-
+    console.log("THE NE CONATCT" + newContact);
+    //Add New contact to list
+    contactList.push(newContact);
+    //Save new List in Local Storage
+    localStorage.setItem("contacts", JSON.stringify(contactList));
+    //Refresh The HTML listview
+    displayAllContacts(contactList);
+    //Clear all inputs
     clearFormInputs();
+    
+    window.location.href = '#contactsPage';
+}
+
+function appendContactToListView(newContact) {
+
+
 }
 
 function cancelAdding() {
     clearFormInputs();
 }
-
-
 
 function clearFormInputs() {
     $('#nameField').val("");
@@ -91,6 +148,8 @@ function clearFormInputs() {
     $('#emailField').val("");
     $('#genderField').val("male").change();
 }
+
+
 
 //============== VALIDATIONS======================
 
